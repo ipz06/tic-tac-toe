6824e61a0fee8536faba150d1c13e18b45f411e1
ipz06 <it.vania.9@gmail.com>
2023-10-29 18:58:34 +0200

    Add User input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,36 @@
-const Player = ({name, symbol}) => {
+import {useState} from 'react';
+
+const Player = ({initialName, symbol}) => {
+
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleClickEditing = () => {
+        setIsEditing(prev => !prev);
+    };
+
+    const handleChange = (event) => {
+        console.log(event);
+        setPlayerName(event.target.value);
+    };
+
+    let editablePlayerName = <span className='player-name'>{playerName}</span>;
+    let btnCaption = 'Edit';
+
+    if (isEditing) {
+        editablePlayerName = <input type='text' required value={playerName} onChange={handleChange}/>;
+        btnCaption = 'Save'; 
+    }
+
+    
 
     return (
         <li>
             <span className='player'>
-              <span className='player-name'>{name}</span>
+                {editablePlayerName}
               <span className='player-symbol'>{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleClickEditing}>{btnCaption}</button>
           </li>
     )
 };
